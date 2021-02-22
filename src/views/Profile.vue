a6a57bd0e92ffdcf0b7700c12ce878bf4f9c2610
Alexey Zimarev <alex@zimarev.com>
2021-02-22 15:21:05 +0100

    Fix the profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,19 +3,19 @@
     <div class="row align-items-center profile-header">
       <div class="col-md-2 mb-3">
         <img
-          :src="auth.user.picture"
+          :src="user.picture"
           alt="User's profile picture"
           class="rounded-circle img-fluid profile-picture"
         />
       </div>
       <div class="col-md text-center text-md-left">
-        <h2>{{ auth.user.name }}</h2>
-        <p class="lead text-muted">{{ auth.user.email }}</p>
+        <h2>{{ user.name }}</h2>
+        <p class="lead text-muted">{{ user.email }}</p>
       </div>
     </div>
 
     <div class="row">
-      {{ JSON.stringify(auth.user, null, 2) }}
+      {{ JSON.stringify(user, null, 2) }}
     </div>
   </div>
 </template>
